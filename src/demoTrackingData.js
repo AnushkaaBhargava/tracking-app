09b1d37d2f0a6d2911480d339ff85df4af2e3976
unknown <anushkaab2005@gmail.com>
2025-06-06 12:45:40 +0530

    App.jsc

---
+++ src/demoTrackingData.js
@@ -0,0 +1,344 @@
+// src/demoTrackingData.js
+
+const demoTrackingInfo = {
+  "success": true,
+  "statusCode": 200,
+  "message": "Tracking info fetched successfully",
+  "data": {
+    "trackingReport": [
+      {
+        "ScanDetail": {
+          "ScanDateTime": "2025-05-17T11:20:57.979",
+          "ScanType": "UD",
+          "Scan": "Manifested",
+          "Status DateTime": "2025-05-17T11:20:57.979",
+          "Scanned Location": "Roorkee_Dc (Uttarakhand)",
+          "StatusCode": "X-UCI",
+          "Instructions": "Manifest uploaded"
+        }
+      },
+      {}, // Empty object in original data
+      {
+        "ScanDetail": {
+          "ScanDateTime": "2025-05-17T13:16:45",
+          "ScanType": "UD",
+          "Scan": "In Transit",
+          "StatusDateTime": "2025-05-17T13:16:45",
+          "Scanned Location": "Roorkee_Dc (Uttarakhand)",
+          "StatusCode": "X-PPOM",
+          "Instructions": "Shipment picked up"
+        }
+      },
+      {}, // Empty object in original data
+      {}, // Empty object in original data
+      {
+        "ScanDetail": {
+          "ScanDateTime": "2025-05-17T17:59:26.703",
+          "ScanType": "UD",
+          "Scan": "In Transit",
+          "StatusDateTime": "2025-05-17T17:59:26.703",
+          "Scanned Location": "Roorkee_Dc (Uttarakhand)",
+          "StatusCode": "X-PIOM",
+          "Instructions": "Shipment Recieved at Origin Center"
+        }
+      },
+      {
+        "ScanDetail": {
+          "ScanDateTime": "2025-05-17T18:00:18.38",
+          "ScanType": "UD",
+          "Scan": "In Transit",
+          "Status DateTime": "2025-05-17T18:00:18.38",
+          "Scanned Location": "Roorkee_Dc (Uttarakhand)",
+          "StatusCode": "X-DBL1F",
+          "Instructions": "Added to Bag"
+        }
+      },
+      {
+        "ScanDetail": {
+          "ScanDateTime": "2025-05-18T10:30:12.57",
+          "ScanType": "UD",
+          "Scan": "In Transit",
+          "StatusDateTime": "2025-05-18T10:30:12.57",
+          "Scanned Location": "Roorkee_Dc (Uttarakhand)",
+          "StatusCode": "X-DLL2F",
+          "Instructions": "Bag Added To Trip"
+        }
+      },
+      {}, // Empty object in original data
+      {
+        "ScanDetail": {
+          "ScanDateTime": "2025-05-18T11:44:51.626",
+          "ScanType": "UD",
+          "Scan": "In Transit",
+          "Status DateTime": "2025-05-18T11:44:51.626",
+          "Scanned Location": "Roorkee_Dc (Uttarakhand)",
+          "StatusCode": "X-OLL4F",
+          "Instructions": "Vehicle Departed"
+        }
+      },
+      {}, // Empty object in original data
+      {}, // Empty object in original data
+      {}, // Empty object in original data
+      {
+        "ScanDetail": {
+          "ScanDateTime": "2025-05-18T13:40:19.756",
+          "ScanType": "UD",
+          "Scan": "In Transit",
+          "Status DateTime": "2025-05-18T13:40:19.756",
+          "Scanned Location": "Dehradun_Badowala_H (Uttarakhand)",
+          "StatusCode": "X-ILL2F",
+          "Instructions": "Trip Arrived"
+        }
+      },
+      {
+        "ScanDetail": {
+          "ScanDateTime": "2025-05-18T13:59:25.05",
+          "ScanType": "UD",
+          "Scan": "In Transit",
+          "StatusDateTime": "2025-05-18T13:59:25.05",
+          "Scanned Location": "Dehradun_Badowala_H (Uttarakhand)",
+          "StatusCode": "X-ILL1F",
+          "Instructions": "Bag Received at Facility"
+        }
+      },
+      {
+        "ScanDetail": {
+          "ScanDateTime": "2025-05-18T15:35:39.421",
+          "ScanType": "UD",
+          "Scan": "In Transit",
+          "Status DateTime": "2025-05-18T15:35:39.421",
+          "Scanned Location": "Dehradun_Badowala_H (Uttarakhand)",
+          "StatusCode": "X-IBD3F",
+          "Instructions": "Shipment Received at Facility"
+        }
+      },
+      {
+        "ScanDetail": {
+          "ScanDateTime": "2025-05-18T15:39:26.058",
+          "ScanType": "UD",
+          "Scan": "In Transit",
+          "Status DateTime": "2025-05-18T15:39:26.058",
+          "ScannedLocation": "Dehradun_Badowala_H (Uttarakhand)",
+          "StatusCode": "X-DBL1F",
+          "Instructions": "Added to Bag"
+        }
+      },
+      {
+        "ScanDetail": {
+          "ScanDateTime": "2025-05-18T16:16:54.713",
+          "ScanType": "UD",
+          "Scan": "In Transit",
+          "Status DateTime": "2025-05-18T16:16:54.713",
+          "Scanned Location": "Dehradun_Badowala_H (Uttarakhand)",
+          "StatusCode": "X-DLL2F",
+          "Instructions": "Bag Added To Trip"
+        }
+      },
+      {
+        "ScanDetail": {
+          "ScanDateTime": "2025-05-18T18:43:09.6",
+          "ScanType": "UD",
+          "Scan": "In Transit",
+          "Status DateTime": "2025-05-18T18:43:09.6",
+          "Scanned Location": "Dehradun_Badowala_H (Uttarakhand)",
+          "StatusCode": "X-OLL4F",
+          "Instructions": "Vehicle Departed"
+        }
+      },
+      {}, // Empty object in original data
+      {}, // Empty object in original data
+      {}, // Empty object in original data
+      {
+        "ScanDetail": {
+          "ScanDateTime": "2025-05-19T01:27:42.029",
+          "ScanType": "UD",
+          "Scan": "In Transit",
+          "Status DateTime": "2025-05-19T01:27:42.029",
+          "Scanned Location": "Noida_Bairangpur_GW (Uttar Pradesh)",
+          "StatusCode": "X-ILL2F",
+          "Instructions": "Trip Arrived"
+        }
+      },
+      {
+        "ScanDetail": {
+          "ScanDateTime": "2025-05-19T03:39:48.655",
+          "ScanType": "UD",
+          "Scan": "In Transit",
+          "Status DateTime": "2025-05-19T03:39:48.655",
+          "ScannedLocation": "Noida_Bairangpur_GW (Uttar Pradesh)",
+          "StatusCode": "X-ILL1F",
+          "Instructions": "Bag Received at Facility"
+        }
+      },
+      {
+        "ScanDetail": {
+          "ScanDateTime": "2025-05-19T04:41:17",
+          "ScanType": "UD",
+          "Scan": "In Transit",
+          "Status DateTime": "2025-05-19T04:41:17",
+          "Scanned Location": "Noida_Bairangpur_GW (Uttar Pradesh)",
+          "StatusCode": "X-DWS",
+          "Instructions": "System weight captured"
+        }
+      },
+      {
+        "ScanDetail": {
+          "ScanDateTime": "2025-05-19T04:41:17",
+          "ScanType": "UD",
+          "Scan": "In Transit",
+          "Status DateTime": "2025-05-19T04:41:17",
+          "Scanned Location": "Noida_Bairangpur_GW (Uttar Pradesh)",
+          "StatusCode": "X-IBD3F",
+          "Instructions": "Shipment Received at Facility"
+        }
+      },
+      {}, // Empty object in original data
+      {}, // Empty object in original data
+      {
+        "ScanDetail": {
+          "ScanDateTime": "2025-05-19T11:58:28.779",
+          "ScanType": "UD",
+          "Scan": "In Transit",
+          "Status DateTime": "2025-05-19T11:58:28.779",
+          "Scanned Location": "Noida_Bairangpur_GW (Uttar Pradesh)",
+          "StatusCode": "X-DBL1F",
+          "Instructions": "Added to Bag"
+        }
+      },
+      {}, // Empty object in original data
+      {}, // Empty object in original data
+      {
+        "ScanDetail": {
+          "ScanDateTime": "2025-05-19T23:29:57.837",
+          "ScanType": "UD",
+          "Scan": "In Transit",
+          "StatusDateTime": "2025-05-19T23:29:57.837",
+          "Scanned Location": "Noida_Bairangpur_GW (Uttar Pradesh)",
+          "StatusCode": "X-DLL2F",
+          "Instructions": "Bag Added To Trip"
+        }
+      },
+      {
+        "ScanDetail": {
+          "ScanDateTime": "2025-05-20T01:56:43.753",
+          "ScanType": "UD",
+          "Scan": "In Transit",
+          "Status DateTime": "2025-05-20T01:56:43.753",
+          "Scanned Location": "Noida_Bairangpur_GW (Uttar Pradesh)",
+          "StatusCode": "X-OLL4F",
+          "Instructions": "Vehicle Departed"
+        }
+      },
+      {
+        "ScanDetail": {
+          "ScanDateTime": "2025-05-20T02:50:50.556",
+          "ScanType": "UD",
+          "Scan": "In Transit",
+          "Status DateTime": "2025-05-20T02:50:50.556",
+          "Scanned Location": "Noida_Sector63D_I (Uttar Pradesh)",
+          "StatusCode": "X-ILL2F",
+          "Instructions": "Trip Arrived"
+        }
+      },
+      {
+        "ScanDetail": {
+          "ScanDateTime": "2025-05-20T03:14:17.168",
+          "ScanType": "UD",
+          "Scan": "In Transit",
+          "Status DateTime": "2025-05-20T03:14:17.168",
+          "ScannedLocation": "Noida_Sector63D_I (Uttar Pradesh)",
+          "StatusCode": "X-ILL1F",
+          "Instructions": "Bag Received at Facility"
+        }
+      },
+      {
+        "ScanDetail": {
+          "ScanDateTime": "2025-05-20T05:07:09.939",
+          "ScanType": "UD",
+          "Scan": "In Transit",
+          "Status DateTime": "2025-05-20T05:07:09.939",
+          "ScannedLocation": "Noida_Sector63D_I (Uttar Pradesh)",
+          "StatusCode": "CS-104",
+          "Instructions": "Outbound against permanent connection [custody scan]"
+        }
+      },
+      {
+        "ScanDetail": {
+          "ScanDateTime": "2025-05-20T07:47:10.55",
+          "ScanType": "UD",
+          "Scan": "Pending",
+          "StatusDateTime": "2025-05-20T07:47:10.55",
+          "Scanned Location": "Noida_Sector63D_D (Uttar Pradesh)",
+          "StatusCode": "X-IBD3F",
+          "Instructions": "Shipment Received at Facility"
+        }
+      },
+      {
+        "ScanDetail": {
+          "ScanDateTime": "2025-05-20T08:20:36.897",
+          "ScanType": "UD",
+          "Scan": "Dispatched",
+          "StatusDateTime": "2025-05-20T08:20:36.897",
+          "Scanned Location": "Noida_Sector63D_D (Uttar Pradesh)",
+          "StatusCode": "X-DDD3FD",
+          "Instructions": "Out for delivery"
+        }
+      },
+      {}, // Empty object in original data
+      {
+        "ScanDetail": {
+          "ScanDateTime": "2025-05-20T10:42:12.002",
+          "ScanType": "UD",
+          "Scan": "Dispatched",
+          "call_duration": 35,
+          "StatusDateTime": "2025-05-20T10:42:12.002",
+          "geo_location": {
+            "lat": 28.62622534,
+            "long": 77.3792922
+          },
+          "ScannedLocation": "Noida_Sector63D_D (Uttar Pradesh)",
+          "StatusCode": "ST-114",
+          "Instructions": "Call placed to consignee"
+        }
+      },
+      {
+        "ScanDetail": {
+          "ScanDateTime": "2025-05-20T10:45:12.174",
+          "ScanType": "UD",
+          "Scan": "Dispatched",
+          "Status DateTime": "2025-05-20T10:45:12.174",
+          "ScannedLocation": "Noida_Sector63D_D (Uttar Pradesh)",
+          "StatusCode": "PL-105",
+          "Instructions": "Paid through link"
+        }
+      },
+      {
+        "ScanDetail": {
+          "ScanDateTime": "2025-05-20T10:45:25.002",
+          "ScanType": "DL",
+          "Scan": "Delivered",
+          "call_duration": 35,
+          "StatusDateTime": "2025-05-20T10:45:25.002",
+          "geo_location": {
+            "lat": 28.6258556,
+            "long": 77.3792671
+          },
+          "Scanned Location": "Noida_Sector63D_D (Uttar Pradesh)",
+          "StatusCode": "EOD-38",
+          "Instructions": "Delivered to consignee"
+        }
+      }
+    ],
+    "status": {
+      "Status": "Delivered",
+      "StatusLocation": "Noida_Sector63D_D (Uttar Pradesh)",
+      "StatusDateTime": "2025-05-20T10:45:25.002",
+      "RecievedBy": "",
+      "StatusCode": "EOD-38",
+      "StatusType": "DL",
+      "Instructions": "Delivered to consignee"
+    }
+  }
+};
+
+export default demoTrackingInfo;
